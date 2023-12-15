f95d8e0027fdc3caf3435832a962af60681b8358
diegoguizanlopez <diegoguizanlopez@users.noreply.github.com>
2023-12-15 16:47:40 +0100

    Damas

--- IA/Modelo_IA/Manejo_de_Árboles_y_Grafos/Ejercicios/Grafos/LasDamas.py
+++ IA/Modelo_IA/Manejo_de_Árboles_y_Grafos/Ejercicios/Grafos/LasDamas.py
@@ -1,9 +1,31 @@
 from Grafos.Grafo import GrafoAvanzado
 
 class GrafoDamas(GrafoAvanzado):
-    
-    def genera_elementos(self,nColumn):
-        v=[]
-        for x in range(nColumn):
-            v.append([i for i in range(nColumn)])
+
+    def __init__(self, n):
+        self.poblacion = []
+        pass
+
+    # generar nelem individuos al azar
+    def genera_nelementos(self, nelem):
+      elementos=[]
+      for i in range(nelem):
+        elementos.append(range(0,2))
+      return elementos
+
+    # valora cuantos errores tiene la posición
+    # 0 es ningún error, cualquier otro valor es positivo
+    def valora_errores_posicion(self, pos):
+      nerrores = 0
+
+      return nerrores
+
+
+#Generar una población inicial
+#durante un número máximo de iteraciones:
+#  ordenar la poblacion
+#  quedarse con los mejores
+#  si alguno es solucion acabar
+#  cruzar individuos
+#  añadir hijos a la población
             
