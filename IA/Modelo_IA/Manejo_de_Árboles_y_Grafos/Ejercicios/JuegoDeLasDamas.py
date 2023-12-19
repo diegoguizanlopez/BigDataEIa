6c1cab2035be8e7752c8a1e9ff514d5d7b29e77d
diegoguizanlopez <diegoguizanlopez@users.noreply.github.com>
2023-12-19 17:39:44 +0100

    Damas

--- IA/Modelo_IA/Manejo_de_Árboles_y_Grafos/Ejercicios/JuegoDeLasDamas.py
+++ IA/Modelo_IA/Manejo_de_Árboles_y_Grafos/Ejercicios/JuegoDeLasDamas.py
@@ -1,6 +1,5 @@
 from Grafos.LasDamas import GrafoDamas
 
-gDamas = GrafoDamas(4)
+gDamas = GrafoDamas(5)
 gDamas.recorre_grafo(nodo_inicial=''.join(str(v) for v in gDamas.poblacion),modo="anchura")
 gDamas.pinta_tablero()
-#gDamas.recorre_grafo(nodo_inicial='[0, 0, 1, 0, 0][0, 1, 0, 0, 0][1, 0, 0, 0, 0][0, 0, 0, 0, 1][0, 0, 0, 1, 0]',modo="anchura")
