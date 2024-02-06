6bc503d20bf217cba2e7b8b2e6422fb8b82fbc7c
diegoguizanlopez <diegoguizanlopez@users.noreply.github.com>
2024-02-06 20:51:02 +0100

    Doc

--- IA/Sistemas_Aprendizaje_Automático/2ºTrimestre/Regresión_Multivariable/Prediccion_Calidad_Vino/Clases/Matplot/BoxPlot.py
+++ IA/Sistemas_Aprendizaje_Automático/2ºTrimestre/Regresión_Multivariable/Prediccion_Calidad_Vino/Clases/Matplot/BoxPlot.py
@@ -4,6 +4,19 @@ from matplotlib import pyplot as plt
 class BoxPlot:
 
     def box_plot(types,dataFrame,by=None,deepColor="Black",faceColor="Black",color="Black",ballsColor="Gray"):
+        """BOX PLOT 
+        
+        By DiegoGL
+
+        Args:
+            types (List): Lista de datos
+            dataFrame (DataFrame): DataFrame de los datos
+            by (Columna): Columna que se comparará en el By. Defaults to None.
+            deepColor (str, optional): Color del fondo.
+            faceColor (str, optional): Face color.
+            color (str, optional): Color fondo 2.
+            ballsColor (str, optional): Color de las bolas.
+        """
         num_cols = len(types)
         num_rows = num_cols // 2 if num_cols % 2 == 0 else num_cols // 2 + 1
         fig, axs = plt.subplots(num_rows, 2, figsize=(10, num_rows*5),squeeze=False)
