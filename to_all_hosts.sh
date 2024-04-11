5c9abefb01666ae9748b9984ad066b3458f25b4a
Diego Guizán López <122787498+diegoguizanlopez@users.noreply.github.com>
2024-04-11 18:32:50 +0200

    Update hosts

--- to_all_hosts.sh
+++ to_all_hosts.sh
@@ -1,3 +1,11 @@
+sudo rm /home/cesgaxuser/.ssh/known_hosts
+
+for servidor in $(cat /etc/hosts|grep Xuwir); 
+	do ssh-keyscan -H $servidor; 
+done >> /home/cesgaxuser/.ssh/known_hosts
+
 clush -l cesgaxuser -bw Xuwira05-[2-4] --copy /etc/hosts --dest /home/cesgaxuser
 clush -l cesgaxuser -bw Xuwira05-[2-4] sudo -S mv /home/cesgaxuser/hosts /etc/hosts
 
+clush -l cesgaxuser -bw Xuwira05-[1-4] sudo reboot
+reboot
